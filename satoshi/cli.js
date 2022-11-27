5bad74e30427c5566796239680cc9273a6ec315b
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2022-11-27 16:50:47 -0600

    verify

--- satoshi/cli.js
+++ satoshi/cli.js
@@ -1,21 +1,10 @@
-import { decodeRawTransaction, getRawTransaction } from './bitcoin.js'
+import { decodeRawTransaction, getRawTransaction, getBlock, getHashOfBlock, rpc } from './bitcoin.js'
 import upto36288 from './upto36288.json' assert { type: "json" }
 import patoshi from './patoshi.json' assert { type: "json" }
 
-const patoshiSet = new Set(patoshi)
-const withInfo = []
-for (const {height, ...data} of upto36288) {
-  // if (tx.length >= 2) {
-  //   console.log(`found ${tx.length} transactions for block #${height}`, { extraNonce, hash, tx, data })
-  //   for (const txx of tx)
-  //     console.log('tx', txx, await getRawTransaction(txx), await decodeRawTransaction(txx))
-  //   break
-  // }
-  withInfo.push({
-    ...data,
-    height,
-    patoshi: patoshiSet.has(height)
-  })
-}
+const { tx: [satoshiCoinbaseTx], hash } = upto36288[9]
 
-console.log(JSON.stringify(withInfo, null, 2))
+// verify btc works
+console.log(await getBlock(await getHashOfBlock(0)))
+console.log(await getBlock(hash))
+console.log(await rpc('gettxout', [satoshiCoinbaseTx, 0]))
